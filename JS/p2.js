e4a6da13622ebda7d2210cb801fb02d00a1cb508
RosCaD <l22550310@chihuahua2.tecnm.mx>
2024-11-18 18:21:31 -0600

    p2

--- JS/p2.js
+++ JS/p2.js
@@ -2,6 +2,7 @@ let nombre = prompt("¿Cuál es tu nombre completo?");
 let edad = prompt ("¿Cuál es tu edad?");
 let pais = prompt ("¿Cuál es tu pais de residencia?");
 alert("Hola " + nombre + ", usted acaba de registrar que cuenta con la edad de  " + edad + " años, además de contar con un lugar de residencia en " + pais  +". Usted ha sido registrad@");
+
 let v = prompt("¿Los datos ingresados son correctos?");
 if (v == "Si" ){
     alert("Registro guardado correctamente:" + nombre + ", " + edad + " años, " + pais);
