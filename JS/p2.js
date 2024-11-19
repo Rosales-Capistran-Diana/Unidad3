6e6aac9005197b13e25e0178967834bccc7d0dcc
RosCaD <l22550310@chihuahua2.tecnm.mx>
2024-11-19 16:13:02 -0600

    p2 actualizao

--- JS/p2.js
+++ JS/p2.js
@@ -4,7 +4,7 @@ let pais = prompt ("¿Cuál es tu pais de residencia?");
 alert("Hola " + nombre + ", usted acaba de registrar que cuenta con la edad de  " + edad + " años, además de contar con un lugar de residencia en " + pais  +". Usted ha sido registrad@");
 
 let v = prompt("¿Los datos ingresados son correctos?");
-if (v == "Si" ){
+if (v =="SI" || v == "Si" || v =="si"){
     alert("Registro guardado correctamente:" + nombre + ", " + edad + " años, " + pais);
 }else{
     alert("Registro borrado."); 
