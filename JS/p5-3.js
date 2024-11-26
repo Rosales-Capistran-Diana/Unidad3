bbeef429a5725af4c69ad88eefd7c931a560d142
RosCaD <l22550310@chihuahua2.tecnm.mx>
2024-11-26 16:17:25 -0600

    p5

--- JS/p5-3.js
+++ JS/p5-3.js
@@ -1,15 +1,15 @@
 let cantNum = Number(prompt("¿Cuántos números deseas ingresar?"));
 let numeros =[];
 for(let i=0; i <cantNum; i++){
-    let num = Number(prompt("Ingresa el nombre de la fruta #" + (i + 1));)
+    let num = Number(prompt("Ingresa el número #" + (i + 1)));
 numeros.push (num);
 }
 function prom(){
     let suma = 0;
-    for(let i=0; i <cantCali; i++){
-        suma = suma+ Number(cali[i]);
+    for(let i=0; i <cantNum; i++){
+        suma = suma+ Number(numeros[i]);
     }
-    let promedio= suma/cantCali;
+    let promedio= suma/cantNum;
     return promedio;
 }
 alert("Los números ingresados fueron: " + numeros.join(", ") +"\nEl promedio final es: " + prom());
