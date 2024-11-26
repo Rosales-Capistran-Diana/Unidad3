4442f195a748586e9f2d4c4b412493983ddcfeac
RosCaD <l22550310@chihuahua2.tecnm.mx>
2024-11-26 00:11:26 -0600

    p4 de nuevo

--- JS/p4-2.js
+++ JS/p4-2.js
@@ -1,14 +1,24 @@
 let cali = confirm ("¿Desea añadir calificaciones?")
 if (cali){
     let horario = {
-    Materia1: prompt("Ingrese la calificación para Lenguajes y autómatas I"),
-    Materia2: prompt("Ingrese la calificación para Arquitectura de computadoras"), 
-    Materia3: prompt("Ingrese la calificación para Telecomunicaciones"),
-    Materia4: prompt("Ingrese la calificación para Taller de bases de datos"),
-    Materia5: prompt("Ingrese la calificación para Fundamentos de ingeniería de SW)",
-    Materia6: prompt("Ingrese la calificación para Sistemas Operativos"),
-    Materia7: prompt("Ingrese la calificación para Programación Web"),
+    Materia1: Number(prompt("Ingrese la calificación para Lenguajes y autómatas I")),
+    Materia2: Number(prompt("Ingrese la calificación para Arquitectura de computadoras")), 
+    Materia3: Number(prompt("Ingrese la calificación para Telecomunicaciones")),
+    Materia4: Number(prompt("Ingrese la calificación para Taller de bases de datos")),
+    Materia5: Number(prompt("Ingrese la calificación para Fundamentos de ingeniería de SW")),
+    Materia6: Number(prompt("Ingrese la calificación para Sistemas Operativos")),
+    Materia7: Number(prompt("Ingrese la calificación para Programación Web")),
+
+    sumarMat: function(){
+        return this.Materia1 + this.Materia2 + this.Materia3 + this.Materia4 + this.Materia5 + this.Materia6 + this.Materia7; 
+    },
+    prom: function(){
+    return this.sumarMat()/7;
+    }
+
     };
+
+    alert("La suma de sus calificaciones es: " + horario.sumarMat() + "\nSu promedio final es: " + horario.prom());
 } else{
     alert("Vuelva pronto :)");
 }
