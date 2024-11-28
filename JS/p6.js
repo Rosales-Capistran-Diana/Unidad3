7811d6ebbc20b5675c63a08040b76ff58fa70c19
RosCaD <l22550310@chihuahua2.tecnm.mx>
2024-11-27 22:44:29 -0600

    a

--- JS/p6.js
+++ JS/p6.js
@@ -1,10 +1,11 @@
-function modificar(){
+function modificar() {
     let titulo = document.querySelectorAll(".grid h3");
-    titulo.forEach(elemento => {
-        elemento.innerHTML = "Adivine qué práctica es";
+        titulo.forEach(elemento => {
+            elemento.innerHTML = "Adivine qué práctica es";
+    });
 
-    let caja = document.getElementById(".bento");
-        caja.style.backgroundColor= "red";
+    let caja = document.getElementById("bento"); // o querySelector(".bento") si es clase
+        caja.style.backgroundColor = "red";
         caja.style.color = "black";
-        caja.style.padding= "30px";
+        caja.style.padding = "30px";
 }
